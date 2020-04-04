1656bdffd4131f16a031e2db791ecafaa595cba7
kovsharov <sanforg2@gmail.com>
2020-04-04 09:19:49 +0300

    Retrieve name of the movie

--- src/main/java/fetcher/provider/MoviesSu.java
+++ src/main/java/fetcher/provider/MoviesSu.java
@@ -7,17 +7,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 public class MoviesSu {
+    private static final Logger log = LoggerFactory.getLogger(MoviesSu.class);
     public static final Map<String, String> ADDITIONAL_HEADERS = Collections.singletonMap("Referer", "https://vidcloud9.com/");
+    public static final String NAME_CSS_SELECTOR = "div .mvic-desc h3";
+    public static final String DEFAULT_MOVIE_NAME = "DefaultMovieName";
     public static final int IFRAME_WAIT = 15;
     private WebDriver webDriver;
-
 
     public MoviesSu(){
         init();
@@ -30,6 +35,16 @@ public class MoviesSu {
     private String getEpisodeRef(String downloadUrl){
         webDriver.get(downloadUrl);
         return webDriver.findElement(By.cssSelector(".thumb.mvi-cover")).getAttribute("href");
+    }
+
+    private String getMovieName(){
+        try {
+            WebElement name = webDriver.findElement(By.cssSelector(NAME_CSS_SELECTOR));
+            return name.getText();
+        }catch (NoSuchElementException e){
+            log.error("Movie name not found using defalut name", e);
+            return DEFAULT_MOVIE_NAME;
+        }
     }
     public void downloadAndSave(String downloadUrl) throws IOException {
 
@@ -46,7 +61,7 @@ public class MoviesSu {
 
         WebElement source = webDriver.findElement(By.id("iframe-embed"));
         String url = source.getAttribute("src");
-
+        String movieName = getMovieName();
         webDriver.get(url);
         WebElement webElement = webDriver.findElement(By.id("myVideo"));
         webElement.click();
@@ -66,7 +81,7 @@ public class MoviesSu {
         int index = netData.indexOf("https://m2x.vidcloud9.com/");
         String first = netData.substring(index, netData.indexOf(",", index));
         URL max = new MoviesSuQualityResolver(new URL(first)).getMaxQualityUrl();
-        MovieSuDownloader downloader = new MovieSuDownloader("file.ts", max);
+        MovieSuDownloader downloader = new MovieSuDownloader(movieName + ".ts", max);
         downloader.download();
 
     }
